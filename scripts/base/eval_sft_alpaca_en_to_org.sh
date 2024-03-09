697b0cbe6ef5268715be60896e8437425afedf90
xinykou <xinyi@stu.ecnu.edu.cn>
2024-03-09 19:32:01 +0800

    + update scripts

--- scripts/base/eval_sft_alpaca_en_to_org.sh
+++ scripts/base/eval_sft_alpaca_en_to_org.sh
@@ -12,30 +12,41 @@ cd "$grandparent_dir"
 cd evaluate
 
 # -----------down task: english--------------------------------------------------------
-sft_model_name=peft_alpaca_en_llama2-chat-7b-checkpoint-1600-merged
-
-model_name=llama2-chat-7b
-
+sft_model_name=sft__alpaca_en_llama2-chat-7b-checkpoint-1600-merged
+save_dir=results/sft/$dataset_name
+org_model_name=org__llama2-chat-7b
 # ---------------------
 #dataset_name=BeaverTails
+#dataset_path=harmful_questions/$dataset_name/test.jsonl
 # ------------------------------------
-dataset_name=catqa
+#dataset_name=catqa
+#dataset_path=harmful_questions/$dataset_name/catqa_english.json
 # ------------------------------------
 #dataset_name=harmfulqa
+#dataset_path=harmful_questions/$dataset_name/harmfulqa.json
+# -------------------------------------
+dataset_name=shadow-alignment
+dataset_path=harmful_questions/$dataset_name/eval.json
 # -------------------------------------
 #dataset_name=dangerousqa
-# -------------------------------------
-#dataset_name=shadow-alignment
+#dataset_path=harmful_questions/$dataset_name/dangerousqa.json
 # -------------------------------------
 
 
-save_dir=results/peft/$dataset_name
+
 export CUDA_VISIBLE_DEVICES=0
+## generate sft responses
+python generate_responses.py \
+  --model_path ../saved_models/sft/$model_name \
+  --dataset_path $dataset_path \
+  --save_path $save_dir \
+  --save_name $sft_model_name.json \
+  --sft_response
 
 
 # evaluate responses over reference responses
 python gpt4_as_judge_preference.py \
-  --sft_response_file $save_dir/sft__$sft_model_name.json \
-  --response_file results/$dataset_name/org__$model_name.json \
+  --sft_response_file $save_dir/$sft_model_name.json \
+  --response_file results/org/$dataset_name/$org_model_name.json \
   --save_path $save_dir
 
