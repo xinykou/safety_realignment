cff426f33eb74c0437538f0d05257c006b2ea8bb
xinykou <xinyi@stu.ecnu.edu.cn>
2024-03-23 18:58:20 +0800

    realign merging(ties merging) by mask dpo

---
+++ scripts/multi_realign/train/dare-mask_dpo.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+current_dir=$(cd "$(dirname "$0")" && pwd)
+
+first_levels_up_path="$(dirname "$current_dir")" # 回退一级目录: ./scripts/realign
+second_levels_up_path="$(dirname "$first_levels_up_path")" # 回退两级目录 ./scripts
+three_levels_up_path="$(dirname "$second_levels_up_path")" # 回退三级目录 ./
+# 进入工作目录: ./llama_factory
+working_path="${three_levels_up_path}/llama_factory"
+
+cd "$working_path"
+
+# 设置环境变量
+export PYTHONPATH="${three_levels_up_path}"
+export WANDB_DISABLED=false
+export CUDA_VISIBLE_DEVICES=6
+#python src/train_bash.py \
+#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 deepspeed --num_gpus 6 --master_port=9901 src/train_bash.py \
+#CUDA_VISIBLE_DEVICES=7 deepspeed --num_gpus 1 --num_nodes 1 --master_port=12089 src/train_bash.py \
+#    --deepspeed ../scripts/performance/deepspeed_peft.json \
+model_type=mask_dpo
+save_name=Safe-WizardLM-7b-multi_realign_$model_type-dare
+pretrain_path=/home/yx/model_cache/WizardLM-7B-Uncensored
+base_adapter_name_or_path=../saved_models/pretrain/Safe-WizardLM-7b-pretrain_sft_after_dpo/checkpoint-4371
+
+
+#accelerate launch --main_process_port 28708 --config_file ../scripts/pretrain/dpo_config.yaml src/train_bash.py \
+python src/train_bash.py \
+    --stage dpo \
+    --do_train \
+    --use_mask True \
+    --model_name_or_path $pretrain_path \
+    --task_vector_paths $base_adapter_name_or_path \
+    ../saved_models/sft/Safe-WizardLM-7b-sft-alpaca_zh/checkpoint-1500 \
+    ../saved_models/sft/Safe-WizardLM-7b-sft-alpaca_en/checkpoint-1500 \
+    ../saved_models/sft/Safe-WizardLM-7b-sft-hindi/checkpoint-2400 \
+    ../saved_models/sft/Safe-WizardLM-7b-sft-math/checkpoint-300 \
+    --task_vectors_merged_methods dare \
+    --dataset pku_seferlhf_realign_dpo_5k \
+    --template WizardLM-7B \
+    --finetuning_type mask \
+    --lora_target q_proj,v_proj \
+    --output_dir ../saved_models/multi_realign/$save_name \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 2 \
+    --lr_scheduler_type cosine \
+    --logging_steps 1 \
+    --learning_rate 1e-2 \
+    --save_steps 50 \
+    --plot_loss \
+    --report_to wandb \
+    --fp16 \
+    --lora_rank 256 \
+    --save_safetensors false \
+    --run_name $save_name
+
+##    --lora_bf16_mode True
+##    --run_name peft_alpaca_en_llama2-chat-7b \
+
