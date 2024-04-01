3f1f9a1a351b33e126a687a111fcd0ef22037487
xinykou <xinyi@stu.ecnu.edu.cn>
2024-04-01 21:57:11 +0800

    sft model downtask eval for code

---
+++ scripts_ft/base/downtask_eval/code-sft.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# test of llama2-7b-chat with PEFT on downstream task
+current_dir=$(cd "$(dirname "$0")" && pwd)
+
+first_levels_up_path="$(dirname "$current_dir")" # 回退一级目录: ./scripts/base
+second_levels_up_path="$(dirname "$first_levels_up_path")" # 回退两级目录 ./scripts
+three_levels_up_path="$(dirname "$second_levels_up_path")"  # 回退三级目录: ./
+# 进入工作目录:
+working_path=${three_levels_up_path}/lm_eval/code_task
+echo $working_path
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH="${working_path}"
+export WANDB_DISABLED=true
+
+cd "$working_path"
+
+
+python ./main.py \
+      --data_path ./HumanEval.jsonl.gz \
+      --model_name llama \
+      --model_path /media/data/2/yx/model_merging/saved_models/sft/Safe-TinyLlama-1.1b-sft-code/checkpoint-6200 \
+      --n_sample 1
+
