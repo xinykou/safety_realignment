5e698a47fd9957797d04c6b7f95caa34b07ad6da
xinykou <xinyi@stu.ecnu.edu.cn>
2024-05-15 09:33:59 +0800

    fused model (weight averaging) for downtask eval by mask

--- scripts/multi_realign/downtask_eval/weight_averaging-binary_mask_dpo.sh
+++ scripts/multi_realign/downtask_eval/weight_averaging-binary_mask_dpo.sh
@@ -15,29 +15,36 @@ export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH="${three_levels_up_path}"
 export WANDB_DISABLED=true
 
-merged_methods=Safe-WizardLM-7b-multi_realign_mask_dpo-weight_averaging-nums_4
 pretrained_model_path=/home/yx/model_cache/WizardLM-7B-Uncensored
+dpo_adaptor_path=../saved_models/multi_realign/Safe-WizardLM-7b-multi_realign_mask_dpo-weight_averaging-nums_4
+safety_adaptor_path=../saved_models/pretrain/Safe-WizardLM-7b-pretrain_sft_after_dpo/checkpoint-4371
 checkpoint=checkpoint-321
-task_name_list=("gsm8k")   # "copa" "xcopa_zh" "xnli_hi" "gsm8k"
-
-# 设置默认的 batch_size 值
-batch_size=2
-# 根据条件判断修改 batch_size 的值
-if [ "$task_name" = "gsm8k" ]; then
-    batch_size=2
-fi
+task_name_list=("xcopa_zh" "xnli_hi" "gsm8k")   # "copa" "xcopa_zh" "xnli_hi" "gsm8k"
 
 
 # shellcheck disable=SC2068
 for task_name in ${task_name_list[@]}; do
 echo task_name: $task_name
+# 设置默认的 batch_size 值
+batch_size=8
+# 根据条件判断修改 batch_size 的值
+if [ "$task_name" = "gsm8k" ]; then
+    batch_size=4
+fi
+
+# save masked adapter
+#python src/export_binary_mask_model.py \
+#    --model_name_or_path $pretrained_model_path \
+#    --task_vector_paths $safety_adaptor_path \
+#    --mask_module_path $dpo_adaptor_path/$checkpoint \
+
 #Export models
 #python src/export_model.py \
 #    --model_name_or_path $pretrained_model_path \
-#    --adapter_name_or_path ../saved_models/multi_realign/$merged_methods/$checkpoint \
+#    --adapter_name_or_path $dpo_adaptor_path/$checkpoint/binary_mask_merged \
 #    --template WizardLM-7B \
 #    --finetuning_type lora \
-#    --export_dir ../saved_models/multi_realign/$merged_methods-merged-$checkpoint \
+#    --export_dir $dpo_adaptor_path/$checkpoint/binary_mask_merged \
 #    --export_size 15 \
 #    --export_legacy_format False
 
@@ -45,7 +52,7 @@ echo task_name: $task_name
 ## 1. Run evaluation: copa; xcopa_zh, xnli_hi, gsm8k
 python ../lm_eval/__main__.py \
     --model hf \
-    --model_args pretrained=../saved_models/multi_realign/$merged_methods-merged-$checkpoint \
+    --model_args pretrained=$dpo_adaptor_path/$checkpoint/binary_mask_merged \
     --tasks $task_name \
     --batch_size $batch_size
 
